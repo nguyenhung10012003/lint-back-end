475ff4afeedb470906afdb1ca0a536056e4a8655
Nguyễn Đức Hùng <71268621+nguyenhung10012003@users.noreply.github.com>
2024-08-07 10:07:51 +0000

    feat: add update post function and delete post function

--- apps/main-app/src/post/model/post.dto.ts
+++ apps/main-app/src/post/model/post.dto.ts
@@ -1,10 +1,22 @@
 import { $Enums } from '@prisma/prisma-main-client';
+import { Type } from 'class-transformer';
+import { IsArray, IsOptional } from 'class-validator';
 
 export interface PostDto {
   content?: string;
-  views?: number;
-  share?: number;
   tags?: string[];
   sourceId?: string;
+  scope?: $Enums.PostScope;
+}
+
+export class PostUpdateDto {
+  @IsOptional()
+  content?: string;
+  @IsOptional()
+  @IsArray()
+  @Type(() => String)
+  tags?: string[];
+  @IsOptional()
+  @Type(() => String)
   scope?: $Enums.PostScope;
 }
