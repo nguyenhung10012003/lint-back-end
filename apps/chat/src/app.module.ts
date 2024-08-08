7c62275b16724bb66eb97c9fcb259192968aec8b
Nguyễn Đức Hùng <71268621+nguyenhung10012003@users.noreply.github.com>
2024-08-08 03:49:38 +0000

    Initial chat module

---
+++ apps/chat/src/app.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@app/common';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [CommonModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
